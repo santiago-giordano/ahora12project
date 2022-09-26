f2c9e76309b177f62dbb91c638cb0ff2cd665c2e
santiago-giordano <100297383+santiago-giordano@users.noreply.github.com>
2022-09-26 16:50:47 -0300

    ahora12-v1
    Source files: (https://datos.gob.ar/dataset/produccion-programa-ahora-12).
    Cleaned and prepped on Excel, Joined on SQL, Imported to Dataframe on Python.

---
+++ QueryJoinedTable.sql
@@ -0,0 +1,13 @@
+
+# Se crea una vista a partir de la "inner join" de las tablas "facturacion" y "operaciones"
+"CREATE VIEW opyfact
+AS SELECT operaciones.mes_anio, operaciones.provincia, operaciones.rubro, operaciones.operaciones, facturacion.facturacion
+FROM operaciones
+INNER JOIN facturacion
+ON operaciones.id= facturacion.id
+WHERE operaciones.mes_anio LIKE '%2021';"
+
+# Se crea una tabla a partir de la vista creada anteriormente
+"SELECT *
+INTO opyfacttable
+FROM dbo.opyfact"
