7a5cb0d497675069c77e8b901490a1618e6b4601
santiago-giordano <100297383+santiago-giordano@users.noreply.github.com>
2022-09-26 16:52:37 -0300

    Update QueryJoinedTable.sql

--- QueryJoinedTable.sql
+++ QueryJoinedTable.sql
@@ -1,13 +1,13 @@
 
 # Se crea una vista a partir de la "inner join" de las tablas "facturacion" y "operaciones"
-"CREATE VIEW opyfact
+CREATE VIEW opyfact
 AS SELECT operaciones.mes_anio, operaciones.provincia, operaciones.rubro, operaciones.operaciones, facturacion.facturacion
 FROM operaciones
 INNER JOIN facturacion
 ON operaciones.id= facturacion.id
-WHERE operaciones.mes_anio LIKE '%2021';"
+WHERE operaciones.mes_anio LIKE '%2021';
 
 # Se crea una tabla a partir de la vista creada anteriormente
-"SELECT *
+SELECT *
 INTO opyfacttable
-FROM dbo.opyfact"
+FROM dbo.opyfact
